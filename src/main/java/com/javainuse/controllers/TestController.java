24d9e667f5bc25f4e021de23ba4b02de8ed58e17
kanakgoswami876 <kanakgoswami@gmail.com>
2022-01-19 22:19:12 -0500

    second version  of program

--- src/main/java/com/javainuse/controllers/TestController.java
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -1,6 +1,5 @@
 package com.javainuse.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,9 +7,9 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class TestController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping("/hello")
 	public ModelAndView firstPage() {
-		return new ModelAndView("welcome");
+		return new ModelAndView("newwelcom");
 	}
 
 }
